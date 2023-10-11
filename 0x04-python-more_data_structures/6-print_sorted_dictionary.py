ddedf4fddd2464b4ceff18e0460156dfa1030b08
Al7assan <al7assan.r@gmail.com>
2023-10-12 00:53:10 +0300

    c 6.1

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,5 +2,5 @@
 def print_sorted_dictionary(a_dictionary):
     copy = a_dictionary.copy()
     copy = dict(sorted(a_dictionary.items()))
-    for i , x in copy.items():
+    for i, x in copy.items():
         print(f"{i}: {x}")
