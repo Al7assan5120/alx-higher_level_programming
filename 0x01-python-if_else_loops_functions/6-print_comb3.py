3e1af084c29e9d1d07a13597b83c5a3a2223b101
Al7assan <al7assan.r@gmail.com>
2023-10-15 03:00:55 +0300

    c 6.0

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for a in range(0, 9):
+    for j in range(a + 1, 10):
+        print("{}{}".format(a, j), end ="")
+        if a == 8 and j == 9:
+            print()
+        else:
+            print(", ", end="")
