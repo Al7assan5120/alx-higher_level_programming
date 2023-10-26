629aedadc1944e3c9aff1894d1cffecad8e2ef6c
Al7assan <al7assan.r@gmail.com>
2023-10-26 05:05:08 +0300

    c 3.1

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    import hidden_4
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+    from sys import argv as av
+    ac = len(av) - 1
+    i = 1
+    a = 0
+    while i <= ac:
+        a = a + int(av[i])
+        i = i + 1
+    print("{:d}".format(a))
