4abe199492354f0e06be8a5cacebeccaf3c668bf
Al7assan <al7assan.r@gmail.com>
2023-11-07 05:36:34 +0200

    c 9.1

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,46 +1,19 @@
 #!/usr/bin/python3
-"""Python Script."""
+"""Rectangle"""
 
-
-class BaseGeometry:
-    """BaseGeometry."""
-
-    def area(self):
-        """area."""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """integer validator.
-
-        Args:
-            name (str): str
-            value (int): int
-        """
-        if type(value) is not int:
-            raise TypeError("{:s} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Rectangle class inheriting from the base geometry class."""
-
+    """Rectangle Calss"""
     def __init__(self, width, height):
-        """Validate and initialize width and height.
-
-        Args:
-            width (int): width
-            height (int): height
-        """
-        if not super().integer_validator("width", width):
-            self.__width = width
-        if not super().integer_validator("height", height):
-            self.__height = height
-
-    def area(self):
-        """Calculate Area."""
-        return self.__width * self.__height
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
 
     def __str__(self):
-        """Return string."""
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+        return (f"[Rectangle] {self.__width}/{self.__height}")
+
+    def area(self):
+        return (self.__width * self.__height)
