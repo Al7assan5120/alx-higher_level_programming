34c3a1105fdf94fe7e5c8f2fdb01de11fa17a2cb
Al7assan <al7assan.r@gmail.com>
2023-11-07 04:49:04 +0200

    c 2.0

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""kjkjk"""
+
+
+def is_same_class(obj, a_class):
+    """_summary_
+
+    Args:
+        obj (_type_): _description_
+        a_class (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return isinstance(obj, a_class)
