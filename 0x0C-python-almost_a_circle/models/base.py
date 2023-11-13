58e6d60eda29b50148295d57dd96aaa4447df6b7
Al7assan <al7assan.r@gmail.com>
2023-11-14 00:26:09 +0200

    c 1.2

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""python sc."""
+
+"""Base of my project"""
 
 
 class Base:
     """Base class"""
 
-    __nb_objects = 0
-    def __init__(self, id=None):
+    nb_objects = 0
 
-        """init method"""
+    def init(self, id=None):
         if id is not None:
             self.id = id
         else:
-            Base.__nb_objects += 1
+            Base.nb_objects += 1
             self.id = Base.__nb_objects
