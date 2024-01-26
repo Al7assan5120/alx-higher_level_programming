ccc3315d79978d2dc8fa4185993acfea1ea01f6e
Al7assan <al7assan.r@gmail.com>
2024-01-26 23:33:41 +0200

    c 2.1

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL, sends a request to the URL
-and displays the value of the X-Request-Id variable
-found in the header of the response"""
+"""Python script that takes in a URL and an email, sends a POSTrequest
+to the passedURL with the email as a parameter, and displays
+the body of the response (decoded in utf-8"""
 
 import urllib
 import sys
