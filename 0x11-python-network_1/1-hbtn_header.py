024653a9181d2aac27fbdf6ce8a0c125a7f5e463
Al7assan <al7assan.r@gmail.com>
2024-01-26 22:31:50 +0200

    c 1.6

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-# Python script that fetches https://alx-intranet.hbtn.io/status
-
+"""Python script that takes in a URL, sends a request to the URL
+and displays the value of the X-Request-Id variable
+found in the header of the response"""
 
 import urllib
 import sys
@@ -10,4 +11,3 @@ if __name__ == "__main__":
     with urlopen(sys.argv[1]) as response:
         request_id = response.getheader("X-Request-Id")
         print(request_id)
-
