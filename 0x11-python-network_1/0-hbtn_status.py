cffcf756e78f8df537915f5312a0d2ddf5ffe883
Al7assan <al7assan.r@gmail.com>
2024-01-26 18:52:56 +0200

    c 0.2

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,7 +8,7 @@ from urllib.request import urlopen
 with urlopen("https://alx-intranet.hbtn.io/status") as respons:
     body = respons.read()
     types = type(body)
-    charset = respons.headers.get_content_charset()
+    charset = body.decode()
     respons.close()
 
 print((f"Body response:\n\t- type: {types}\n\t- content: {body}\n"
