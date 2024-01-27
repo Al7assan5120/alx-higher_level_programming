45a9e2f5e848b6f377b60cd139e35e58a8c86e37
Al7assan <al7assan.r@gmail.com>
2024-01-27 11:21:20 +0200

    c 4.3

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,5 +6,4 @@ import requests
 
 if __name__ == '__main__':
     respons = requests.get("https://alx-intranet.hbtn.io/status")
-    types = type(respons.text)
-    print(f"Body response:\n\t- type: {types}\n\t- content: {respons.text}")
+    print(f"Body response:\n\t- type: {type(respons.text)}\n\t- content: {respons.text}")
