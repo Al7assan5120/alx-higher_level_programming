8e1eeb31113dbd7b9a7b2f817b4cfce2f18cd958
Al7assan <al7assan.r@gmail.com>
2024-01-27 13:18:01 +0200

    c 7.0

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL, sends a request to the URL
+and displays the body of the response (decoded in utf-8)"""
+
+import requests
+from requests.exceptions import HTTPError
+import sys
+
+if __name__ == '__main__':
+
+    url = sys.argv[1]
+
+    try:
+        body = requests.get(url)
+        print(body.raise_for_status())
+    except HTTPError as error:
+        print(f'Error code: {error.status}')
